580689c8d7644f50bab2bbe2d4dcaaf0abb56592
Nora Galvin <nora@gempages.help>
2019-08-05 10:50:47 +0700

    update code

--- socket.go
+++ socket.go
@@ -22,7 +22,7 @@ const (
 	// Time allowed to read the next pong message from the peer.
 	pongWait = 60 * time.Second
 	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
+	pingPeriod = 25 * time.Second
 	// Maximum message size allowed from peer.
 	maxMessageSize = 1024
 )
